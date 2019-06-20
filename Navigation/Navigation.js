9c4c895a7f4abef4860f38dad5b99a611b0acb8d
Raigyo <vincent.chilot@gmail.com>
2019-06-20 16:22:54 +0200

    add icons

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,6 +1,8 @@
 // Navigation/Navigation.js
 
+import React from 'react'
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation'
+import { StyleSheet, Image } from 'react-native';
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
 import Favorites from '../Components/Favorites'
@@ -19,10 +21,40 @@ const SearchStackNavigator = createStackNavigator({
 
 const MoviesTabNavigator = createBottomTabNavigator({
   Search: {
-    screen: SearchStackNavigator
+    screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => { //add icon
+          return <Image
+            source={require('../Images/ic_search.png')}
+            style={styles.icon}/> // style to resize
+        }
+      }
   },
   Favorites: {
-    screen: Favorites
+    screen: Favorites,
+    navigationOptions: {
+      tabBarIcon: () => {//add icon
+        return <Image
+          source={require('../Images/ic_favorite.png')}
+          style={styles.icon}/>
+      }
+    }
+  }
+},
+  {
+    tabBarOptions: {
+      activeBackgroundColor: '#DDDDDD', // background color for selected tabs
+      inactiveBackgroundColor: '#FFFFFF', // background color for unselected tabs
+      showLabel: false, // hide titles
+      showIcon: true // TabNavigator will display defined icons only
+    }
+  }
+)
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
   }
 })
 export default createAppContainer(MoviesTabNavigator)
